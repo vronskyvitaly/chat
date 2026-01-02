34907251d8e043b11e1c86af99ad34cd45f740b0
vitalyvronsky <vronskyvitaly@mail.ru>
2026-01-02 21:13:46 +0300

    fix: signUp page and onSubmit function

--- src/common/pages/sign-up-page/index.tsx
+++ src/common/pages/sign-up-page/index.tsx
@@ -1,37 +1,66 @@
 'use client'
-import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { PATH } from '@/common/constants'
 import { useRouter } from 'next/navigation'
 import { FaArrowLeft, FaUser } from 'react-icons/fa'
 import { SignUpForm } from '@/features/auth/ui/sign-up-form/ui'
+import { useSignUpMutation } from '@/features/auth/api'
 import type { TSignUpFormValues } from '@/features/auth/ui/sign-up-form/types'
 
 const SignUpPage = () => {
   const router = useRouter()
   const [error, setError] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [signUpMutation] = useSignUpMutation()
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null)
+
+  // Очистка таймаута при размонтировании компонента
+  useEffect(() => {
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+      }
+    }
+  }, [timeoutId])
 
   const onSubmit = async (data: TSignUpFormValues) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+      setTimeoutId(null)
+    }
+
     setError(null)
     setIsLoading(true)
 
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_EXPRESS_SERVER}api/auth/register`, {
-        name: data.name.trim(),
-        email: data.email.trim(),
-        password: data.password
-      })
-
+      await signUpMutation(data).unwrap()
       // Если регистрация успешна, перенаправляем на страницу входа
-      if (response.status === 201) {
-        router.push(PATH.SING_IN)
-      }
+      router.push(PATH.SING_IN)
     } catch (err: unknown) {
-      if (axios.isAxiosError(err)) {
-        setError(err.response?.data?.message || 'Ошибка регистрации')
+      if (err && typeof err === 'object' && 'data' in err) {
+        const errorData = err as { data: { message?: string } }
+        // todo: 500 [client || server]
+        const errorMessage =
+          errorData.data.message === 'Пользователь с таким email уже зарегистрирован'
+            ? 'A user with this email is already registered'
+            : 'Registration error'
+        setError(errorMessage)
+
+        // Устанавливаем новый таймаут и сохраняем его ID
+        const id = setTimeout(() => {
+          setError(null)
+          setTimeoutId(null)
+        }, 4000)
+
+        setTimeoutId(id)
       } else {
-        setError(err instanceof Error ? err.message : 'Ошибка регистрации')
+        setError('Registration error')
+        const id = setTimeout(() => {
+          setError(null)
+          setTimeoutId(null)
+        }, 4000)
+
+        setTimeoutId(id)
       }
     } finally {
       setIsLoading(false)
