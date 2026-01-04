f8f2fd7a78e2e697a0afd78554309acebb86dec0
vitalyvronsky <vronskyvitaly@mail.ru>
2026-01-04 21:57:58 +0300

    fix 11: SignOutButton

--- src/features/auth/ui/sign-out-button/index.tsx
+++ src/features/auth/ui/sign-out-button/index.tsx
@@ -1,10 +1,15 @@
 import { signOut } from 'next-auth/react'
 import { PATH } from '@/common/constants'
+import { useRouter } from 'next/navigation'
 
 export const SignOutButton = () => {
+  const router = useRouter()
   return (
     <button
-      onClick={() => async () => await signOut({ callbackUrl: PATH.SING_IN, redirect: false })}
+      onClick={() => async () => {
+        await signOut({ callbackUrl: PATH.SING_IN, redirect: false })
+        router.push('/')
+      }}
       className='flex h-14 items-center justify-center gap-3 px-8 bg-white text-gray-700 font-semibold rounded-full shadow-lg hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 border border-gray-200 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:border-gray-700 transition-all duration-200'
     >
       <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
